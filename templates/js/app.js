3acc085081aebce0966d655f26fb4185e4365eba
Connor <connormakhlouta@gmail.com>
2018-02-18 19:33:56 +0200

    chore(*) - add more structure for templates and db - WIP

---
+++ templates/js/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/users', users);
+
+module.exports = app;
--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,18 @@
 const fs = require('fs');
 
 module.exports = {
-    copy_template: (from, to) =>  {
-    from = path.join(__dirname, '..', 'templates', from);
-    write(to, fs.readFileSync(from, 'utf-8'));
+    write_route_from_template: (to, name) =>  {
+    let from = path.join(__dirname, '..', 'templates/js', 'route.js');
+    let data = fs.readFileSync(from, 'utf-8')
+    let result = data.replace(/string to be replaced/g, 'replacement');
+
+
+    // module.exports.write(to, );
+    },
+
+
+    write: (path, str, mode) => {
+    fs.writeFileSync(path, str, { mode: mode || 0o666 });
+    console.log('   \x1b[36mcreate\x1b[0m : ' + path);
     }
 }
